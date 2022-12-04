8cd2085e45872bfb52ae668afec1a947f43ea0b4
Adrian Fernandez Castro <adrian.fernandez.castro@rehau.com>
2022-12-04 14:59:58 +0100

    adjustment

--- src/app/index.mjs
+++ src/app/index.mjs
@@ -5,7 +5,7 @@ export default async (app, _opts) => {
 	app.get('/', async (_req, reply) => {
 		reply.code(200).type('text/html; charset=utf-8').send('<code>https://embed.pkmn.dev/:id<br>// :id is the species name of the Pokémon</code><br><br><code>https://embed.pkmn.dev/:id?sprite=value<br>// the sprite query parameter value can be one of the following: <br> // shiny, back, shiny&back, back&shiny</code>');
 	});
-// respond to every /:id request with the embed
+	// respond to every /:id request with the embed
 	app.get('/:id', async (req, reply) => {
 		// get the id from the request
 		const { id } = req.params;
