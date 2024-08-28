04504a99d3c05a4eb51a096445385dada5d0c404
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2024-08-28 10:44:07 +0200

    fix: remove duplicate field

--- src/app/fetch.mjs
+++ src/app/fetch.mjs
@@ -27,7 +27,6 @@ export async function fetchMon(id) {
 			      		base
 			      		percentageWithOrdinaryPokeballAtFullHealth
 			    	}
-			    	classification
 				baseStats {
 					attack
 					defense
